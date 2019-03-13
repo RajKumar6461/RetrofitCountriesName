a2eaadd5351d1137cc8724dde1466b542620b5ac
RajKumar6461 <raj.kumar@jungleworks.com>
2019-03-13 19:58:31 +0530

    1st Commit

---
+++ app/src/main/java/com/example/countriesretrofit/Country.java
@@ -0,0 +1,13 @@
+package com.example.countriesretrofit;
+
+public class Country {
+    private String name;
+
+    public Country(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+}
---
+++ app/src/main/java/com/example/countriesretrofit/CountryAdapterRecycleView.java
@@ -0,0 +1,75 @@
+package com.example.countriesretrofit;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CountryAdapterRecycleView extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+    private ArrayList<Country> countryArrayList;
+    public  CountryAdapterRecycleView(final ArrayList<Country> countries){
+        this.countryArrayList=countries;
+    }
+
+    /**
+     * This method used to create view and inflate xml file and return StudentViewHolder Object
+     *
+     * @param viewGroup
+     * @param i
+     * @return studentViewHolder
+     */
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.adapter_country,viewGroup,false);
+        StudentViewHolder studentViewHolder = new StudentViewHolder(view)
+                ;       return studentViewHolder;
+    }
+
+    /**
+     * This method used to bind the view with data
+     *
+     * @param viewHolder
+     * @param i
+     */
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+        Country std=countryArrayList.get(i);
+        String title=std.getName();
+        ((StudentViewHolder) viewHolder).mTextViewName.setText(title);
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+
+        return countryArrayList.size();
+    }
+
+    /**
+     * inner class for viewHolder
+     * used set id to textview and also used set Onclicklistener
+     */
+
+    class StudentViewHolder extends RecyclerView.ViewHolder{
+        private TextView mTextViewName;
+        RelativeLayout relativeLayout;
+        public StudentViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            //set ids to both textview
+            mTextViewName=itemView.findViewById(R.id.textName);
+
+            //setting OnclickListener to item
+        }
+    }
+}
+
